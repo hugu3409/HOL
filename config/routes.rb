bf494ac451bf27e64f2a42c99d4909ced4a178e2
3402 <3402@example.com>
2023-11-07 11:22:48 +0900

    route.rbにloginを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :situations
   resources :classrooms
   resources :teachers
+  get 'login/show', to: 'login#show'
+  post 'login/login', to: 'login#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
