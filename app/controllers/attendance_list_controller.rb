04d43c49b23b7061a50859bee1845ddab1f83e48
3409 <3409@example.com>
2023-11-17 15:12:03 +0900

    出欠一覧名前にリンク完了

--- app/controllers/attendance_list_controller.rb
+++ app/controllers/attendance_list_controller.rb
@@ -2,8 +2,13 @@ class AttendanceListController < ApplicationController
   def show
     @situations = Situation.all  #どのテーブル使うか
     @attendances = Attendance.all
+
+    @classrooms = Classroom.all.order(:grade, :number) #コントローラーで学年、クラス順に並べてプルダウンに表示できるようになる(Id順じゃない)
     @classroom_id = params[:classroom_id]
     @today = Date.today
+
+    #@students = Student.all
+    @students = Student.where(:classroom_id => params[:classroom_id]).order(number: :asc)
   end
 
   def select_classroom
