43a50af3b0bdfc613e070da1d570166befcf481c
3405 <3405@example.com>
2024-01-10 12:28:00 +0900

    出欠入力確認完了たぶん

--- app/models/situation.rb
+++ app/models/situation.rb
@@ -1,12 +1,16 @@
 class Situation < ApplicationRecord
 
     def self.target_list
-        criteria = ["欠席", "出席停止", "遅刻", "早退"]
+        criteria = ["欠席", "出席停止", "遅刻", "早退", "不明"]
         Situation.where(name: criteria)
     end
 
     def self.target_id_list
         Situation.target_list.map(&:id)
+    end
+
+    def fumei?
+        id == Situation.find_by(name: "不明").id
     end
 
 end
